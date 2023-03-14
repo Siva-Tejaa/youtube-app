cfd472145c28d028340593927cf75b16eda839b3
Siva-Tejaa <asivateja1999@gmail.com>
2023-03-14 16:50:19 +0530

    first commit

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,10 @@
-// export const GOOGLE_API_KEY = "AIzaSyB9UXf97Q3wgx2sNlihA0ZCTuT4o6YiPp0"
+export const GOOGLE_API_KEY = "AIzaSyB9UXf97Q3wgx2sNlihA0ZCTuT4o6YiPp0";
 // export const GOOGLE_API_KEY = "AIzaSyBTKH7-fdCQIZpgq8BDcF6RWuYKCPfugZc"
+// export const GOOGLE_API_KEY = "AIzaSyBYQ8FCyrkXb9ZTVD1Gdes0T9ix_9jXL68"
 
-export const GOOGLE_API_KEY = "AIzaSyBYQ8FCyrkXb9ZTVD1Gdes0T9ix_9jXL68"
+
+// export const GOOGLE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
 
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`;
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,13 @@
 import React,{useState, useEffect} from 'react'
 import { YOUTUBE_VIDEO_CATEGORY } from '../api/Api';
 
+
+import Carousel from 'react-elastic-carousel';
+
+
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+
+
 
 const Button = () => {
 
@@ -28,12 +34,18 @@ const Button = () => {
   return (
     <div className=''>
 
-
+<Carousel itemPadding={[0, 0]} itemsToShow={5} outerSpacing={5} pagination={false} itemsToScroll={2}>
       {
         category.map((categ, index) => (
-            <button className="px-3 py-2 m-1 max-w-fit  bg-gray-200 rounded-lg" key={index}>{categ?.snippet?.title}</button>
+            // <button className="px-3 py-2 m-1 max-w-fit  bg-gray-200 rounded-lg" key={index}>{categ?.snippet?.title}</button>
+            <button className="px-3 py-2 m-1 w-52 whitespace-nowrap bg-gray-200 rounded-lg" key={index}>{categ?.snippet?.title}</button>
         ))
         }
+        </Carousel>
+  
+
+        
+
         
     </div>
   )
